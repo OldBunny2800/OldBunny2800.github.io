404de3b8461672ae0a31806f613832ffb10a3e91
AceAttorneyMaster111 <NoahS2003@gmail.com>
2017-07-29 21:20:46 -0400

    Reorganized jQuery, personalized HTML/CSS

--- JS/Mom/0.js
+++ JS/Mom/0.js
@@ -1,6 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function()
+{
 	standard()
-	$("#done").click(function(){
-		$("#video").show();
-	});
 });
--- JS/generic.js
+++ JS/generic.js
@@ -1,3 +1,7 @@
 function standard(){
 	$("li:not(:last-of-type)").append(document.createElement("br"));
+	$("#done").click(function(){
+		$("#video").show();
+		$("#done").hide();
+	});
 }
