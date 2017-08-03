cc2bf4d2da2552356a26e556cd70563333b0d3cd
AceAttorneyMaster111 <NoahS2003@gmail.com>
2017-08-03 09:04:16 -0400

    Hopefully fixed #done problem

--- JS/generic.js
+++ JS/generic.js
@@ -1,4 +1,6 @@
 function standard(){
+	$("#video").hide();
+	$(".message").hide();
 	$("li:not(:last-of-type)").append(document.createElement("br"), document.createElement("br"));
 	$("#done").click(function(){
 		$("#video").show();
@@ -12,4 +14,12 @@ function standard(){
 			value: $(this).data("progress-value")
 		}).children(".ui-progressbar-value").html($(this).data("progress-value").toString() + "%");
 	});
+	if($(location).attr("path") != "/Users/Mom/0.html")
+	{
+		$("#done").hide();
+		challenge.done(function()
+		{
+			$("#done").show();
+		});
+	}
 }
