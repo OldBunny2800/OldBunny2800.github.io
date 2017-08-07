d4a4b060a1e44e77f15ab8156af37ba9823662eb
AceAttorneyMaster111 <NoahS2003@gmail.com>
2017-08-06 22:00:54 -0400

    Website built!

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,18 @@
 $(document).ready(function()
 {
 	standard();
+	$("#userform").submit(function() {
+		var username = $("#userform input[name='username']").val();
+		var url = "/Users/" + username + "/user.html"
+		$.ajax({
+			url : url,
+			type : "HEAD",
+			error : function() {
+				alert("User does not exist. Please contact Noah if this is in error.");
+			},
+			success : function() {
+				window.location.replace(url);
+			}
+		});
+	});
 });
