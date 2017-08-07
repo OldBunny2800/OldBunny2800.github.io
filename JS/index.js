244c3fa795c3519893912cc77d4da6d0fff13d89
AceAttorneyMaster111 <NoahS2003@gmail.com>
2017-08-06 22:10:00 -0400

    ...it was a semicolon.

--- JS/index.js
+++ JS/index.js
@@ -3,7 +3,7 @@ $(document).ready(function()
 	standard();
 	$("#userform").submit(function() {
 		var username = $("#userform input[name=username]").val();
-		var url = "http://oldbunny2800.github.io/Users/" + username + "/user.html"
+		var url = "http://oldbunny2800.github.io/Users/" + username + "/user.html";
 		$.ajax({
 			url : url,
 			type : "HEAD",
