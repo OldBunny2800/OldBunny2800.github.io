264460e98f8137978394b18ce0bd110a5401bd73
AceAttorneyMaster111 <NoahS2003@gmail.com>
2017-08-07 12:04:52 -0400

    This might work.

--- JS/index.js
+++ JS/index.js
@@ -4,7 +4,6 @@ $(document).ready(function()
 	$("#userform").submit(function() {
 		var username = $('#userform input[name="username"]').val();
 		var userUrl = "https://oldbunny2800.github.io/Users/" + username + "/user.html";
-		console.log(userUrl); // This might be the problem.
 		$.ajax({
 			url : userUrl,
 			type : "HEAD",
@@ -12,7 +11,7 @@ $(document).ready(function()
 				alert("User does not exist. Please contact Noah if this is in error.");
 			},
 			success : function() {
-				window.location.replace(url);
+				window.location.replace(userUrl);
 			}
 		});
 	});
