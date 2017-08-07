d76598d92bdf8e75f7c67b212f829cbdcecff4a5
AceAttorneyMaster111 <NoahS2003@gmail.com>
2017-08-07 12:00:07 -0400

    Trying to log the value to see the problem.

--- JS/index.js
+++ JS/index.js
@@ -4,6 +4,7 @@ $(document).ready(function()
 	$("#userform").submit(function() {
 		var username = $('#userform input[name="username"]').val();
 		var userUrl = "https://oldbunny2800.github.io/Users/" + username + "/user.html";
+		console.log(userUrl); // This might be the problem.
 		$.ajax({
 			url : userUrl,
 			type : "HEAD",
