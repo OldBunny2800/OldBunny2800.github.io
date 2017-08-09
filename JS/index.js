cb739fdc3ea2996e3a4576c996e523e7fb521428
AceAttorneyMaster111 <NoahS2003@gmail.com>
2017-08-09 11:11:39 -0400

    I think I finally have it.

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,9 @@
 $(document).ready(function()
 {
 	standard();
-	$("#userform").submit(function() {
+	function submitUser() {
 		var username = $('#userform input[name="username"]').val();
 		var userUrl = "https://oldbunny2800.github.io/Users/" + username + "/user.html";
-		/* $.ajax({
-			url : userUrl,
-			type : "HEAD",
-			error : function() {
-				alert("User does not exist. Please contact Noah if this is in error.");
-			},
-			success : function() {
-				window.location.replace(userUrl);
-			}
-		}); */
 		var http = new XMLHttpRequest();
 	    http.open('HEAD', userUrl, false);
 	    http.send();
@@ -22,5 +12,5 @@ $(document).ready(function()
 	    } else {
 	    		window.location = userUrl;
 	    }
-	});
+	}
 });
