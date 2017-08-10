35786e251e6abdbab92b0ed37f9e54bbf351bf16
AceAttorneyMaster111 <NoahS2003@gmail.com>
2017-08-10 19:17:05 -0400

    Wow, I'm stupid again. #scopeproblems

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,16 @@
 $(document).ready(function()
 {
 	standard();
-	function submitUser() {
-		var username = $('#userform input[name="username"]').val();
-		var userUrl = "https://oldbunny2800.github.io/Users/" + username + "/user.html";
-		var http = new XMLHttpRequest();
-	    http.open('HEAD', userUrl, false);
-	    http.send();
-	    if(http.status == 404) {
-	    		alert("User does not exist. Please contact Noah if this is in error.");
-	    } else {
-	    		window.location = userUrl;
-	    }
-	}
 });
+function submitUser() {
+	var username = $('#userform input[name="username"]').val();
+	var userUrl = "https://oldbunny2800.github.io/Users/" + username + "/user.html";
+	var http = new XMLHttpRequest();
+    http.open('HEAD', userUrl, false);
+    http.send();
+    if(http.status == 404) {
+    		alert("User does not exist. Please contact Noah if this is in error.");
+    } else {
+    		window.location = userUrl;
+    }
+}
