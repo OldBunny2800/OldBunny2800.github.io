43de3b3d1a570b44cb47d3d849c59a4cee622cd3
AceAttorneyMaster111 <NoahS2003@gmail.com>
2017-08-10 21:26:38 -0400

    User Claire automatically created.

---
+++ JS/Users/Claire/user.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	standard();
+	/* $("li").each(function() {
+		var link = document.createElement("a");
+		link.setAttribute("href", /* User directory here * / + $(this).index() + ".html")
+		link.innerHTML = $(this).attr("data-lesson-name");
+		$(this).prepend(link);
+	}); */
+})
